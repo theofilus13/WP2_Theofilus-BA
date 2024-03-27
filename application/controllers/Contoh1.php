406b2e644d2ec4e7c0d7ffd81127ec9811bb1b34
theofilus13 <theorrqlemon@gmail.com>
2024-03-27 12:31:22 +0700

    commit pertama

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"nama saya theo, saya dari kelas 15.4A.30
+        hobi saya bermain gitar";
+    }
+}
