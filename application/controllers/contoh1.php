27482ca89e6d43b09cbcf419df14ecdf769bf720
theofilus13 <theorrqlemon@gmail.com>
2024-03-27 15:31:24 +0700

    tugas1

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,7 +4,7 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"nama saya theo, saya dari kelas 15.4A.30
-        hobi saya bermain gitar";
+        echo"<h2>nama saya theo, saya dari kelas 15.4A.30
+        hobi saya bermain gitar</h2>";
     }
 }
