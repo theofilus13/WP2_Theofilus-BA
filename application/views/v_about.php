a1372e610fe1c77e2e93780000837a6442b1976b
LionelTheo10 <theolionel79@gmail.com>
2024-04-22 14:28:16 +0700

    Pertemuan 4 dan Tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+<h1><?php echo $judul ?></h1>
+
+    <h4>Nama</h4>
+        <ul type="disc">
+        <li>Nama Depan : Theofilus</li>
+        <li>Nama Belakang : BA</li>
+        </ul>
+    <br>
+    <h4>Alamat</h4>
+        <ul type="none">
+        <li> Gang Bakau 1 Tanjung Hulu</li>
+        </ul>
+ 
+    <h4>Tempat Lahir</h4>
+        <ul type="none">
+        <li>Natai Panjang</li>
+        </ul>
+    <h4>Olah Raga Favorit</h4>
+         <ul type="square">
+        <li>Bola Voli</li>
+        <li>Bola Kaki</li>
+        </ul>
+</section>
